bd5705d428c65c0dc2333f9a4e691f2a2ea30e56
dsonwane <deepsonwane26@gmail.com>
2023-09-01 18:35:43 +0530

    supporting tools

---
+++ tests/SupportingTools/PlainTextDoc.py
@@ -0,0 +1,12 @@
+"""Example library in plain text format.
+
+- Formatting is not supported.
+- URLs like http://example.com are turned to links.
+- Custom links are not supported.
+- Linking to `My Keyword` works.
+"""
+ROBOT_LIBRARY_DOC_FORMAT = 'text'
+
+
+def my_keyword():
+    """Nothing more to see here."""
